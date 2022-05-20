54aaea4bbba0682bb3a58ed6fb177ce4cd53682d
Zarul Zubir <zarul.izham@gmail.com>
2022-05-20 12:18:26 +0800

    Update bank list

--- src/Commands/UpdateBankListCommand.php
+++ src/Commands/UpdateBankListCommand.php
@@ -8,82 +8,84 @@ use Illuminate\Console\Command;
 
 class UpdateBankListCommand extends Command
 {
-	/**
-	 * The name and signature of the console command.
-	 *
-	 * @var string
-	 */
-	protected $signature = 'fpx:banks';
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'fpx:banks';
 
-	/**
-	 * The console command description.
-	 *
-	 * @var string
-	 */
-	protected $description = 'Update FPX banks List.';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update FPX banks List.';
 
-	/**
-	 * Create a new command instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		parent::__construct();
-	}
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
-	/**
-	 * Execute the console command.
-	 *
-	 * @return int
-	 */
-	public function handle()
-	{
-		$handler = new BankEnquiry;
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $handler = new BankEnquiry;
 
-		$dataList = $handler->getData();
+        $dataList = $handler->getData();
 
 
 
-		try {
-			$response = $handler->connect($dataList);
+        try {
+            $response = $handler->connect($dataList);
 
-			$token = strtok($response, "&");
-			$bankList = $handler->parseBanksList($token);
+            $token = strtok($response, "&");
+            $bankList = $handler->parseBanksList($token);
 
-			if ($bankList === false) {
-				$this->error('We could not find any data');
-				return;
-			}
+            if ($bankList === false) {
+                $this->error('We could not find any data');
+                return;
+            }
 
-			$bar = $this->output->createProgressBar(count($bankList));
-			$bar->start();
+            $bar = $this->output->createProgressBar(count($bankList));
+            $bar->start();
 
-			foreach ($bankList as $key => $status) {
-				$bankId = explode(" - ", $key)[1];
-				$bank = $handler->getBanks($bankId);
-				if (empty($bank)) {
-					logger("Bank Not Found: ", [$bankId]);
-					continue;
-				}
-				Bank::updateOrCreate(['bank_id' => $bankId], [
-					'status' => $status == 'A' ? 'Online' : 'Offline',
-					'name' => $bank['name'],
-					'short_name' => $bank['short_name'],
-					'type' => $bank['type'] ?? [],
-				]);
+            foreach ($bankList as $key => $status) {
+                $bankId = explode(" - ", $key)[1];
+                $bank = $handler->getBanks($bankId);
+                if (empty($bank)) {
+                    logger("Bank Not Found: ", [$bankId]);
+                    continue;
+                }
+                Bank::updateOrCreate(['bank_id' => $bankId], [
+                    'status' => $status == 'A' ? 'Online' : 'Offline',
+                    'name' => $bank['name'],
+                    'short_name' => $bank['short_name'],
+                    'type' => $bank['type'] ?? [],
+                ]);
 
-				$bar->advance();
-			}
 
-			$bar->finish();
-			$this->newLine();
-		} catch (\Exception $e) {
-			logger("Bank Updating failed", [
-				'message' => $e->getMessage(),
-			]);
-			$this->error("request failed due to " . $e->getMessage());
-			throw $e;
-		}
-	}
+
+                $bar->advance();
+            }
+
+            $bar->finish();
+            $this->newLine();
+        } catch (\Exception $e) {
+            logger("Bank Updating failed", [
+                'message' => $e->getMessage(),
+            ]);
+            $this->error("request failed due to " . $e->getMessage());
+            throw $e;
+        }
+    }
 }
